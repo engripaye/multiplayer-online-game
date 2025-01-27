ecfa60373249ece6ee21e3a83e2354b5b8902b95
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-01-27 03:43:40 +0100

    init commit

---
+++ src/main/java/com/ipaye/multiplayeronlinegame/MultiplayerOnlineGameApplication.java
@@ -0,0 +1,13 @@
+package com.ipaye.multiplayeronlinegame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiplayerOnlineGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiplayerOnlineGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ipaye/multiplayeronlinegame/MultiplayerOnlineGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipaye.multiplayeronlinegame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiplayerOnlineGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
